bb66e406683dd389ce9824e3e3c1ff7c01c8c569
Abdallah Mohammed <abdallahabdulzahir@gmail.com>
2025-05-10 02:46:33 +0300

    Initial Commit

---
+++ SwiftModernConcurrencyLearn/SwiftModernConcurrencyLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftModernConcurrencyLearnApp.swift
+//  SwiftModernConcurrencyLearn
+//
+//  Created by name on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftModernConcurrencyLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
